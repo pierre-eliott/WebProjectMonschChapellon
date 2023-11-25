fda5d2c52b77f2371b32f7fb62435c2f3ca8235e
Pierre-Eliott <pierre-eliott.monsch@etu.emse.fr>
2023-11-25 09:41:37 +0100

    Add of endpoint for search

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const station_module_1 = require("./station.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(station_module_1.StationModule);
-    await app.listen(3000);
+    await app.listen(process.env.PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
